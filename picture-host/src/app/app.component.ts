96c9cc13b582706621c5a9c96bcd3b02cd21d6cb
arlindgurgurovci <arlind.gurgurovci@ost.ch>
2024-10-21 17:25:19 +0200

    init auf angular basis

---
+++ picture-host/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MatSlideToggleModule, HeaderComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'picture-host';
+}
+
