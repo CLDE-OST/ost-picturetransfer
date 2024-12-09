a5ed55805741bf8cb0b8c3bfe99a9657f014f69e
arlindgurgurovci <155858954+arlindgurgurovci@users.noreply.github.com>
2024-12-09 10:49:30 +0100

    Revert "Merge pull request #17 from CLDE-OST/shadcnui-integration"
    This reverts commit 08a0f6f6e996835516f4ede8410767c4869cb704, reversing
    changes made to bd2f3431e095c76596d88d85dffb8b387d9265cc.

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <div className="grid grid-cols-12">
   <Navbar className="p-2 my-4 col-start-6 col-end-8 rounded-xl shadow-sm border border-neutral-700 shadow-neutral-800 bg-neutral-800">
-    <Image width={50} height={50} src="/logo.png" alt="hOST" className="mx-auto"/>
+    <Image width={50} height={50} src="/logo.png" alt="hOST" className="mx-auto" />
   </Navbar>
 </div>
   );
